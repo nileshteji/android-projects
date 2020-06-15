d4a47ebecd804b8ca279910317276019ec938fe3
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-06-15 22:25:04 +0530

    Dependency Injection using Dagger2

--- DependencyInjection3/app/src/main/java/com/arcore/dependencyinjection/di/Component.java
+++ DependencyInjection3/app/src/main/java/com/arcore/dependencyinjection/di/Component.java
@@ -5,5 +5,6 @@ import com.arcore.dependencyinjection.MainActivity;
 @dagger.Component(modules = MainActivity.class)
 public interface Component {
     void inject(MainActivity mainActivity);
-    
+
+
 }
