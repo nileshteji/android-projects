d1d1043bab4ef0041c620f047a4d693fdf6e4604
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-06 02:34:22 +0530

    Refractor

--- RxJava/app/src/main/java/com/osos/myapplication/MainActivity.java
+++ RxJava/app/src/main/java/com/osos/myapplication/MainActivity.java
@@ -1,76 +1,58 @@
 package com.osos.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingComponent;
-import androidx.databinding.DataBindingUtil;
-
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.osos.myapplication.databinding.ActivityMainBinding;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-Observable<String> observable;
-ActivityMainBinding mainBinding;
-final static String TAG="Debugging";
+    final static String TAG = "Debugging";
+    Observable<String> observable;
+    ActivityMainBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
-
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mainBinding.text.setOnClickListener(v -> Log.d(TAG, "onCreate: "));
         observable.just("Nilesh").cache()
                 .subscribeOn(Schedulers.io())
-
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
 
-            }
+                    }
 
-            @Override
-            public void onNext(@NonNull String s) {
-                Log.d("Tag","Nilesh");
-             mainBinding.text.setText(s);
-            }
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Log.d("Tag", "Nilesh");
+                        mainBinding.text.setText(s);
+                    }
 
-            @Override
-            public void onError(@NonNull Throwable e) {
+                    @Override
+                    public void onError(@NonNull Throwable e) {
 
-            }
+                    }
 
-            @Override
-            public void onComplete() {
+                    @Override
+                    public void onComplete() {
 
-            }
-        });
-
-
-
+                    }
+                });
 
 
     }
-
-
-
-
-
-
-
-
 
 
 }
