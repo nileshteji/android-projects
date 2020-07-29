07e491e01b91c0b36898e42fe3e06bb909d803f3
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-29 20:18:48 +0530

    Commit

---
+++ HiltAndroidApplication/app/src/main/java/com/example/hiltandroidapplication/HiltApplication.java
@@ -0,0 +1,10 @@
+package com.example.hiltandroidapplication;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HiltApplication extends Application {
+}
