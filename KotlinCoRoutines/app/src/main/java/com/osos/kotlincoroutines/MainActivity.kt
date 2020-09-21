93f4a271d67d760aafad81a38cc5f895af288d8c
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-09-21 21:03:31 +0530

    CorRoutines

--- KotlinCoRoutines/app/src/main/java/com/osos/kotlincoroutines/MainActivity.kt
+++ KotlinCoRoutines/app/src/main/java/com/osos/kotlincoroutines/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.osos.kotlincoroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
-    val TAG="MainActivity"
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,54 +22,57 @@ class MainActivity : AppCompatActivity() {
         // withContext to change the thread of the coroutine
 
 
-
-//
-//        // very important work
-//        GlobalScope.launch(Dispatchers.IO)
-//        {
-//            var dummy=network();
-//            withContext(Dispatchers.Main){
-//                text.text=dummy;
-//            }
-//
-//        }
+    }
 
 
-        // this is when the network call is made one after one
-        GlobalScope.launch(Dispatchers.IO) {
-            val time=measureTimeMillis {
-                network();
-                network2();
+    fun timeOut() {
+        lifecycleScope.launch {
+            val job = withTimeoutOrNull(2000) {
+                timeOutOne();
             }
-            Log.d(TAG, "Time Taken is $time")
-        }
 
-        // making network calls async
-        GlobalScope.launch(Dispatchers.IO) {
-            val time= measureTimeMillis {
-               val job= launch {
-                     network()
+            when (job) {
+                null -> withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Time Out", LENGTH_LONG).show()
                 }
-                val job2=launch {
-                    network2()
+                else -> withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "CoRoutine Done", LENGTH_LONG).show()
                 }
-                job.join()
-                job2.join()
             }
-            Log.d(TAG, "Time taken for async Jobs $time")
         }
-
-
-
 
     }
 
-    suspend fun network():String{
-        delay(3000)
-        return "Nilesh Teji"
+
+    suspend fun timeOutOne() {
+        delay(1000)
     }
 
-    suspend fun network2():String{
+
+    fun exceptionHandle() {
+        var handler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
+            println("Exceptions is Thrown")
+        }
+
+        CoroutineScope(Dispatchers.IO).launch(handler) {
+            //JOB----1
+
+            val JOB = launch {
+                var nilesh: String = network2();
+                Log.d(TAG, "main:$nilesh ")
+            }
+
+            JOB.invokeOnCompletion {
+                when (it) {
+                    null -> Log.d(TAG, "main: JOB Done")
+                    else -> Log.d(TAG, "main: NO SuccesFull")
+                }
+            }
+        }
+    }
+
+
+    suspend fun network2(): String {
         delay(3000)
         return "Nilesh Teji"
     }
