77794f510ba73c7ebfa9c27c97f349e42ddc9630
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-10-25 16:15:59 +0530

    Dialog Addition

--- bUTTONcLICKlISTENER/app/src/main/java/com/example/buttonclicklistener/MainActivity.java
+++ bUTTONcLICKlISTENER/app/src/main/java/com/example/buttonclicklistener/MainActivity.java
@@ -1,34 +1,85 @@
 package com.example.buttonclicklistener;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button obj, obj1;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    final static String TAG="MainActivityLog";
+    TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        obj = findViewById(R.id.button);
-        obj1 = findViewById(R.id.button2);
+        textView=findViewById(R.id.text)
+        Log.d(TAG, "onCreate: ");
 
-        obj.setOnClickListener(this);
-        obj1.setOnClickListener(this);
+
+        Observable.just(1,2,45,6).subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        (d)-> {},
+                        (e)-> Log.d(TAG, "onCreate: e"),
+                        ()-> Log.d(TAG, "onCreate: Done"));
+
+
+
     }
 
     @Override
-    public void onClick(View v) {
-        if(v==obj)
-        {
-            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
-        }
-        if(v==obj1)
-        {
-            Toast.makeText(this, "bye", Toast.LENGTH_SHORT).show();
-        }
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart:");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
